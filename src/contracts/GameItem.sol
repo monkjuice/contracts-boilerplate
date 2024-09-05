f0ad94b7a7e8169dad4beb0431e51e6aac361ca2
monkjuice <agustinbarg@gmail.com>
2024-09-05 12:14:46 -0300

    fix warnings and linting

--- src/contracts/GameItem.sol
+++ src/contracts/GameItem.sol
@@ -2,15 +2,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721, ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GameItem is ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) Ownable(msg.sender) {}
 
-    uint8 currentPhase = 0;
+    uint8 private currentPhase = 0;
 
     function awardItem(address player, string memory tokenURI) public {
         uint256 tokenId = _nextTokenId++;
